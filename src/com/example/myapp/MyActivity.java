794a4ff94b3690d3a1678d00c069601dea5c18e8
florije1988 <florije1988@gmail.com>
2014-08-15 14:21:33 +0800

    这就是这本书的第一个例子……算是recall了一下数据。

--- src/com/example/myapp/MyActivity.java
+++ src/com/example/myapp/MyActivity.java
@@ -2,14 +2,36 @@ package com.example.myapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MyActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class MyActivity extends Activity {
+    Button mTrueButton;
+    Button mFalseButton;
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MyActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton = (Button)findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MyActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
